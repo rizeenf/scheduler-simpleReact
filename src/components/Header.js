5fd46e1843437030e71b8b0bb8c5b3c776342a2d
rizeenf <rize.nf11@gmail.com>
2022-12-25 20:58:48 +0700

    Finishing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom';
 
 const Header = ({ showAddSch, showBtn }) => {
+  const loc = useLocation();
+
   return (
     <div className="container my-3 d-flex justify-content-around align-items-center">
       <h1>College Schedule</h1>
-      <Button text={showAddSch ? 'Close' : 'Add'} classBtn={`btn-secondary btn-lg ${showAddSch ? 'btn-danger': ''}`} onClick={showBtn} />
+      { loc.pathname == '/About' ? '' : <Button text={showAddSch ? 'Close' : 'Add'} classBtn={`btn-secondary btn-lg ${showAddSch ? 'btn-danger': ''}`} onClick={showBtn} />}
     </div>
   )
 }
